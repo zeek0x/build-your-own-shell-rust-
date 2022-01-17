8416e522bde7e8c6c3312323e80d59457d13f0e5
zeek0x <0h23k1.n40k1@gmail.com>
2022-01-18 00:53:10 +0900

    Handling Args

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,20 @@ use std::process::Command;
 
 fn main() {
     loop {
-        // use the `>` character as the prompt
-        // need to explicitly flush this to ensure it prints before read_line
         print!("> ");
         stdout().flush();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let command = input.trim();
+        // everything after the first whitespace character
+        //     is interpreted as args to the command
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
-        let mut child = Command::new(command).spawn().unwrap();
+        let mut child = Command::new(command).args(args).spawn().unwrap();
 
-        // don't accept another command until this one completes
         child.wait();
     }
 }
